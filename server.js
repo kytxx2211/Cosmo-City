a78e3a0b17f177142f264dae15c8807a07ebc689
kytxx2211 <66138632+kytxx2211@users.noreply.github.com>
2026-03-07 04:31:50 +0700

    Implement Discord authentication with Passport.js

---
+++ server.js
@@ -0,0 +1,62 @@
+const express = require("express")
+const session = require("express-session")
+const passport = require("passport")
+const DiscordStrategy = require("passport-discord").Strategy
+
+const app = express()
+
+const CLIENT_ID = "PUT_DISCORD_CLIENT_ID"
+const CLIENT_SECRET = "PUT_DISCORD_SECRET"
+
+passport.serializeUser((user, done) => done(null, user))
+passport.deserializeUser((obj, done) => done(null, obj))
+
+passport.use(new DiscordStrategy({
+  clientID: CLIENT_ID,
+  clientSecret: CLIENT_SECRET,
+  callbackURL: "http://localhost:3000/auth/discord/callback",
+  scope: ["identify"]
+},
+(accessToken, refreshToken, profile, done) => {
+  return done(null, profile)
+}
+))
+
+app.use(session({
+  secret: "cosmo_secret",
+  resave: false,
+  saveUninitialized: false
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.use(express.static("public"))
+
+app.get("/auth/discord",
+ passport.authenticate("discord")
+)
+
+app.get("/auth/discord/callback",
+ passport.authenticate("discord",{failureRedirect:"/"}),
+ function(req,res){
+  res.redirect("/dashboard.html")
+ }
+)
+
+app.get("/api/user",(req,res)=>{
+
+ if(!req.user){
+  return res.json({login:false})
+ }
+
+ res.json({
+  login:true,
+  username:req.user.username
+ })
+
+})
+
+app.listen(3000, ()=>{
+ console.log("Cosmo City running")
+})
